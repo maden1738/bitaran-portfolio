0115c4f532363d557a8624fc822a4339d20af114
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-05-04 08:56:34 +0545

    fix bug

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Research() {
   return <div>Research</div>;
 }
--- src/pages/Teachings.tsx
+++ src/pages/Teachings.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Teachings() {
   return <div>Teachings</div>;
 }
